ca9b24e73361d1049a3fd460a91c69e246797f74
Wolfsblvt <wolfsblvt@gmx.de>
2024-03-25 05:37:32 +0100

    Change swipes list icon to font awesome

--- index.js
+++ index.js
@@ -417,8 +417,7 @@ const addSwipesButton = (mesIdx, isForced = false)=>{
     Array.from(container.querySelectorAll('.mfc--button')).forEach(it=>it.remove());
     const mes = chat[mesIdx];
     const btn = document.createElement('div'); {
-        btn.classList.add('mfc--button');
-        btn.textContent = '▤';
+        $(btn).addClass('mfc--button mes_swipes fa-solid fa-arrows-left-right-to-line');
         btn.title = `View swipes (${mes.swipes?.length ?? 0})`;
         btn.addEventListener('click', async(evt)=>{
             const dom = document.createElement('div'); {
